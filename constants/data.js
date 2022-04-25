36cbf71e0149150a1202f3f494c16a4ba8e663bc
jorge <jorge.stalin@protonmail.com>
2022-04-25 15:47:01 -0500

    My personal data to be consumed in json format

---
+++ constants/data.js
@@ -0,0 +1,51 @@
+const userData = {
+    githubUsername: "taloah",
+    name: "Jorge Sánchez",
+    designation: "Data Engineer",
+    avatarUrl: "https://avatars.githubusercontent.com/u/46788479?v=4",
+    email: "jssc199@gmail.com",
+    phone: "+593 963173429",
+    address: "Guayaquil, Ecuador.",
+    projects: [
+      {
+        title: "Password Generator",
+        link: "https://github.com/taloah/password_generator",
+        imgUrl: "/pwd_gen.png",
+      },
+    ],
+    about: {
+      title:
+        "Hello! It's me! Jorge!",
+      description: [
+        `I'm glad to you be here!`,
+        `I do not know what suppossed to have to write here! :(`,
+        `But, I want to get a job, pls`,
+      ],
+      currentProject: "",
+      currentProjectUrl: "",
+    },
+    experience: [
+      {
+        title: "Graduation",
+        company: "UCSG, Guayaquil.",
+        year: "2022",
+        companyLink: "https://ucsg.edu.ec",
+        desc: "Major degree in Computer Science and Engineering.",
+      },
+      {
+        title: "Freelance",
+        company: "",
+        year: "2021",
+        companyLink: "",
+        desc: "Some stuff of ETL's",
+      },
+    ],
+    resumeUrl:
+      "",
+    socialLinks: {
+      linkedin: "https://linkedin.com/in/jorge-sanchez-castro",
+      github: "https://github.com/taloah",
+    },
+  };
+  
+  export default userData;  
