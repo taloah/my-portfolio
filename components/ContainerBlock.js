883cd9adbda0c0e82dcaf154d0a17744462b4da4
jorge <jorge.stalin@protonmail.com>
2022-04-25 15:44:15 -0500

    Components added

---
+++ components/ContainerBlock.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+export default function ContainerBlock({ children, ...customMeta }) {
+  const router = useRouter();
+
+  const meta = {
+    title: "Jorge Sánchez - Developer and Data Engineer",
+    description: `Get in touch with me to know more.`,
+    image: "/avatar.png",
+    type: "website",
+    ...customMeta,
+  };
+  return (
+    <div>
+      <Head>
+        <title>{meta.title}</title>
+        <meta name="robots" content="follow, index" />
+        <meta content={meta.description} name="description" />
+      </Head>
+      <main className="dark:bg-gray-800 w-full">
+        <Navbar />
+        <div>{children}</div>
+        <Footer />
+      </main>
+    </div>
+  );
+}
